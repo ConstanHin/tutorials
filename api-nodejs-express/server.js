e369d609a07120ce9fbd0264b2ef7d05d6da5449
Albert Mata <hello@albertmata.net>
2021-09-30 14:43:40 +0200

    Afegits arxius tutorial JS: Crear una API amb Node.js i Express

---
+++ api-nodejs-express/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    const time = new Date().toISOString();
+    console.log(`[${time}] Request de tipus ${req.method} rebut`);
+    next();
+});
+
+app.get('/api', (req, res) => {
+    const fs = require('fs');
+    try {
+      const data = fs.readFileSync('data.json', 'utf8');
+      res.send(data);
+    } catch (err) {
+      console.error(err);
+    }
+});
+
+app.listen(3000, () => console.log('API escoltant al port 3000'));
