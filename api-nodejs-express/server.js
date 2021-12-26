496b36df73842f6560e2e44bf8bc3a053f5bd0fc
Constantin Vlad <catalin_vlad_00@yahoo.es>
2021-12-27 00:31:24 +0100

    added throttle and cors

--- api-nodejs-express/server.js
+++ api-nodejs-express/server.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const throttle = require('express-throttle');
 
 app.use(cors());
 
 app.use((req, res, next) => {
-    const time = new Date().toISOString();
-    console.log(`[${time}] Request de tipus ${req.method} rebut`);
-    next();
+  const time = new Date().toISOString();
+  console.log(`[${time}] Request de tipus ${req.method} rebut`);
+  next();
 });
 
-app.get('/api', (req, res) => {
-    const fs = require('fs');
-    try {
-      const data = fs.readFileSync('data.json', 'utf8');
-      res.send(data);
-    } catch (err) {
-      console.error(err);
-    }
+app.get('/api', throttle({ "rate": "10/m" }), (req, res) => {
+  const fs = require('fs');
+  try {
+    const data = fs.readFileSync('data.json', 'utf8');
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(3000, () => console.log('API escoltant al port 3000'));
